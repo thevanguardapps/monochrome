a846e8bb50728c0b8b8d11cb851d2037ec65597c
thevanguardapps <thevanguardapps@protonmail.com>
2022-07-02 17:52:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/vanguard/monochrome/app/home.java
@@ -0,0 +1,116 @@
+package com.vanguard.monochrome.app;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bugsnag.android.Bugsnag;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+
+public class home extends AppCompatActivity {
+
+
+    private TextView privacy;
+    private TextView disclaimer;
+
+    public void gotoSignUpActivity(View view){
+        Intent i=new Intent(getApplicationContext(), signup.class);
+        startActivity(i);
+    }
+
+
+    public void gotoLoginActivity(View view){
+        Intent i=new Intent(getApplicationContext(), login.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        Parse.initialize(this);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        MixpanelAPI mixpanel = MixpanelAPI.getInstance(this, "362bdc538a5dae7f6173abd97beb83ef");
+        Bugsnag.start(this);
+
+        privacy = (TextView) findViewById(R.id.privacy);
+        String privacytext = "Privacy Policy";
+        SpannableString content = new SpannableString(privacytext);
+        content.setSpan(new UnderlineSpan(), 0 , privacytext.length(), 0);
+        privacy.setText(content);
+        privacy.setTextColor(Color.BLUE);
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(home.this);
+                builder.setTitle("Privacy Policy");
+                builder.setMessage("At Monochrome, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Monochrome and how we use it.\n" +
+                        " \n" +
+                        "If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at thevanguardapps@protonmail.com\n" +
+                        " \n" +
+                        "Log Files\n" +
+                        "Monochrome follows a standard procedure of using log files. These files log visitors when they use app. The information collected by log files include internet protocol (IP) addresses, Internet Service Provider (ISP), date and time stamp, device name, operating system version, the configuration of the app. The purpose of the information is for analyzing trends, administering the app, tracking users' movement on the app, and gathering demographic information.\n" +
+                        " \n" +
+                        "Third Party Services\n" +
+                        "The app does use third-party services that may collect information used to identify you. This includes:\n" +
+                        " \n" +
+                        "• Back4App\n" +
+                        "• Mixpanel\n" +
+                        "• Bugsnag\n" +
+                        " \n" +
+                        "Children's Information\n" +
+                        "Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.\n" +
+                        " \n" +
+                        "Monochrome does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our App, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.\n" +
+                        " \n" +
+                        "Consent\n" +
+                        "By using our app, you hereby consent to our Privacy Policy.\n");
+                builder.setPositiveButton("OK", null);
+                builder.show();
+            }
+        });
+
+        disclaimer = (TextView) findViewById(R.id.disclaimer);
+        String disclaimertext = "Disclaimer";
+        SpannableString content1 = new SpannableString(disclaimertext);
+        content1.setSpan(new UnderlineSpan(), 0 , disclaimertext.length(), 0);
+        disclaimer.setText(content1);
+        disclaimer.setTextColor(Color.BLUE);
+        disclaimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(home.this);
+                builder.setTitle("Disclaimer");
+                builder.setMessage("We are doing our best to prepare the content of this app. However, Monochrome cannot warranty the expressions and suggestions of the contents, as well as its accuracy. In addition, to the extent permitted by the law, Monochrome shall not be responsible for any losses and/or damages due to the usage of the information on our app. \n" +
+                        "\n" +
+                        "By using our app, you hereby consent to our disclaimer and agree to its terms.\n" +
+                        "\n" +
+                        "Any links contained in our app may lead to external sites are provided for convenience only. Any information or statements that appeared in these sites or app are not sponsored, endorsed, or otherwise approved by Monochrome. For these external sites, Monochrome cannot be held liable for the availability of, or the content located on or through it. Plus, any losses or damages occurred from using these contents or the internet generally.\n" +
+                        "As long as the app and the information and services on the app are provided free of charge, we will not be liable for any loss or damage of any nature.\n" +
+                        "\n" +
+                        "By using our app, you also agree that you are the only one responsible for the content you post in this app. The developers of Monochrome are not responsible for any of the user-generated content.");
+                builder.setPositiveButton("OK", null);
+                builder.show();
+            }
+        });
+
+        if(ParseUser.getCurrentUser()!=null){
+            Intent i=new Intent(home.this, userlist.class);
+            startActivity(i);
+            finish();
+        }
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+    }
+}
---
+++ app/src/main/java/com/vanguard/monochrome/app/login.java
@@ -0,0 +1,137 @@
+package com.vanguard.monochrome.app;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vanguard.monochrome.app.utils.Utils;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class
+login extends AppCompatActivity implements View.OnClickListener,View.OnKeyListener {
+
+    EditText usernameFieldInLogIn,passwordFieldInLogIn;
+    TextView logInbtn;
+    RelativeLayout rlloginActivityLayout;
+    ImageView logoInLogIn;
+    TextView forgotPassword;
+
+
+    //Toast Method
+    public void alertDisplayer(String title,String message){
+        Toast.makeText(getApplicationContext(),title + " " + message,Toast.LENGTH_LONG).show();
+    }
+
+    //Implements of View.onKeylistener
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_ENTER && event.getAction()==KeyEvent.ACTION_DOWN){
+            loginFunction();
+        }
+        return false;
+    }
+
+
+    //Implemments of View.onClicklistener
+    @Override
+    public void onClick(View view) {
+
+        /*
+        //Hiding KeyBoard
+        if(view.getId()==R.id.logoinLogin || view.getId()==R.id.loginActivityRlayout){
+            InputMethodManager imm=(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }
+        else */
+
+        if(view.getId()==R.id.logInBtn){
+            loginFunction();
+        }
+
+        //Goto ForgotPassword Activity
+        if(view.getId()==R.id.forgotPassword){
+            Intent i=new Intent(getApplicationContext(), forgotpassword.class);
+            startActivity(i);
+        }
+
+        //Cursor Visibility on click on passwordFieldInLogIn
+        else if(view.getId()==R.id.passswordLayout){
+            passwordFieldInLogIn.setCursorVisible(true);
+        }
+        //Cursor Visibility on click on usernameFieldInLogIn
+        else if(view.getId()==R.id.userLayout){
+            usernameFieldInLogIn.setCursorVisible(true);
+        }
+    }
+
+    public void loginFunction() {
+        final String username=usernameFieldInLogIn.getText().toString().trim();
+        final String password=passwordFieldInLogIn.getText().toString().trim();
+
+        if (username.length() == 0 || password.length() == 0)
+        {
+            Utils.showDialog(this, R.string.err_fields_empty);
+        }
+        else {
+            final ProgressDialog dia=ProgressDialog.show(login.this,null,getString(R.string.alert_wait));
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser parseUser, ParseException e) {
+                    dia.dismiss();
+                    if (parseUser != null) {
+                        userlist.user=parseUser;
+                        Log.i("AppInfo","SuccessfulLogIn");
+
+                        //Starting AfterLoginActivity
+                        Intent i=new Intent(getApplicationContext(), userlist.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else {
+                        alertDisplayer("Login failed", e.getMessage()+ "Please retry");
+                    }
+                }
+            });
+        }
+    }
+
+
+    //OnCreate Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        rlloginActivityLayout=findViewById(R.id.loginActivityRlayout);
+        logInbtn= findViewById(R.id.logInBtn);
+        logoInLogIn=findViewById(R.id.groupintenticon);
+
+        usernameFieldInLogIn=findViewById(R.id.usernameInLogIn);
+        passwordFieldInLogIn=findViewById(R.id.passwordInLogIn);
+        forgotPassword=findViewById(R.id.forgotPassword);
+
+        /*
+        rlloginActivityLayout.setOnClickListener(this); //hiding KeyBoard
+        logoInLogIn.setOnClickListener(this);  //Hiding KeyBoard
+         */
+
+        logInbtn.setOnClickListener(this); //Login Button
+        forgotPassword.setOnClickListener(this);  //starting Forgot Activity
+        passwordFieldInLogIn.setOnKeyListener(this);  //Goto login
+        passwordFieldInLogIn.setOnClickListener(this);  //Cursor Visibility
+        usernameFieldInLogIn.setOnClickListener(this); //Cursor Visibility
+        usernameFieldInLogIn.setOnKeyListener(this); //goto password field
+    }
+}
